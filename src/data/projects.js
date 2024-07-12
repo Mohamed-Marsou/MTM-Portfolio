dd19d7164dd648fa098a7766c8d8b9dfe8d21391
MTM <marsoumtm@gmail.com>
2024-07-12 13:00:49 +0100

    images issue1

--- src/data/projects.js
+++ src/data/projects.js
@@ -33,6 +33,76 @@ const projectsData = [
       "../src/assets/projects/Deverda/deverda7.jpg",
       
     ]
+  },
+  {
+    "id": 2,
+    "title": "Deverda - E-commerce Website",
+    "slug": "deverda-ecommerce-website-shopify",
+    "tag": "#Full Stack , #Shopify , #UI/UX",
+    //intro
+    "intro" : "Deverda is a sophisticated e-commerce website developed for a cosmetics brand using Shopify, a leading CMS for commerce. This project allowed me to gain valuable experience with Shopify's extensive capabilities. The main objective was to create the best possible UI/UX while implementing user behavior tracking to ensure the website could be continuously updated to meet user needs. This approach ensured a dynamic, user-centered shopping experience that adapts and improves based on user interactions and feedback.",
+    //description
+    "description": `Deverda is a sophisticated e-commerce website developed for a cosmetics brand using Shopify, a leading CMS for commerce. This project allowed me to gain valuable experience with Shopify and its extensive capabilities. The main objective was to create the best possible UI/UX while also implementing user behavior tracking to ensure the website could be continuously updated to meet user needs.
+
+    In creating Deverda, a sophisticated e-commerce platform for a renowned cosmetics brand, the utilization of Shopify, a premier CMS for commerce, was paramount. This endeavor provided me with significant experience in leveraging Shopify's comprehensive features and capabilities. The primary goal was to design an exceptional UI/UX, coupled with the integration of user behavior tracking mechanisms to facilitate ongoing updates tailored to user preferences.
+    
+    Developing Deverda, an advanced e-commerce site for a high-profile cosmetics brand, involved harnessing the power of Shopify, an industry-leading CMS for commerce. Through this project, I gained profound insights into Shopify's robust functionalities. The core objective was to engineer a superior UI/UX, incorporating user behavior tracking to ensure the website remains dynamically responsive to evolving user needs.`,
+
+    "coverImg": "../src/assets/projects/deverda/logo.jpg",
+    "secondaryImg": "../src/assets/projects/deverda/logo.jpg",
+    "date": "27 June 2023",
+    "hostLink": "https://deverda.com",
+    "githubLink": "",
+    "techIcons":
+    [
+        "../src/assets/images/icons/icons8-shopify-48.png", 
+    ],
+    "images": [
+      "../src/assets/projects/Deverda/deverda8.jpg",
+      "../src/assets/projects/Deverda/deverda4.jpg",
+      "../src/assets/projects/Deverda/deverda1.jpg",
+      "../src/assets/projects/Deverda/deverda2.jpg",
+      "../src/assets/projects/Deverda/deverda3.jpg",
+      "../src/assets/projects/Deverda/deverda5.jpg",
+      "../src/assets/projects/Deverda/deverda6.jpg",
+      "../src/assets/projects/Deverda/deverda7.jpg",
+      
+    ]
+  },
+  {
+    "id": 3,
+    "title": "Deverda - E-commerce Website",
+    "slug": "deverda-ecommerce-website-shopify",
+    "tag": "#Full Stack , #Shopify , #UI/UX",
+    //intro
+    "intro" : "Deverda is a sophisticated e-commerce website developed for a cosmetics brand using Shopify, a leading CMS for commerce. This project allowed me to gain valuable experience with Shopify's extensive capabilities. The main objective was to create the best possible UI/UX while implementing user behavior tracking to ensure the website could be continuously updated to meet user needs. This approach ensured a dynamic, user-centered shopping experience that adapts and improves based on user interactions and feedback.",
+    //description
+    "description": `Deverda is a sophisticated e-commerce website developed for a cosmetics brand using Shopify, a leading CMS for commerce. This project allowed me to gain valuable experience with Shopify and its extensive capabilities. The main objective was to create the best possible UI/UX while also implementing user behavior tracking to ensure the website could be continuously updated to meet user needs.
+
+    In creating Deverda, a sophisticated e-commerce platform for a renowned cosmetics brand, the utilization of Shopify, a premier CMS for commerce, was paramount. This endeavor provided me with significant experience in leveraging Shopify's comprehensive features and capabilities. The primary goal was to design an exceptional UI/UX, coupled with the integration of user behavior tracking mechanisms to facilitate ongoing updates tailored to user preferences.
+    
+    Developing Deverda, an advanced e-commerce site for a high-profile cosmetics brand, involved harnessing the power of Shopify, an industry-leading CMS for commerce. Through this project, I gained profound insights into Shopify's robust functionalities. The core objective was to engineer a superior UI/UX, incorporating user behavior tracking to ensure the website remains dynamically responsive to evolving user needs.`,
+
+    "coverImg": "../src/assets/projects/deverda/logo.jpg",
+    "secondaryImg": "../src/assets/projects/deverda/logo.jpg",
+    "date": "27 June 2023",
+    "hostLink": "https://deverda.com",
+    "githubLink": "",
+    "techIcons":
+    [
+        "../src/assets/images/icons/icons8-shopify-48.png", 
+    ],
+    "images": [
+      "../src/assets/projects/Deverda/deverda8.jpg",
+      "../src/assets/projects/Deverda/deverda4.jpg",
+      "../src/assets/projects/Deverda/deverda1.jpg",
+      "../src/assets/projects/Deverda/deverda2.jpg",
+      "../src/assets/projects/Deverda/deverda3.jpg",
+      "../src/assets/projects/Deverda/deverda5.jpg",
+      "../src/assets/projects/Deverda/deverda6.jpg",
+      "../src/assets/projects/Deverda/deverda7.jpg",
+      
+    ]
   }
   ];
 
