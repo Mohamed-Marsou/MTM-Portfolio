8eeab907738757b9e9406d3f7237c1760a92005e
MTM <marsoumtm@gmail.com>
2024-07-12 13:34:13 +0100

    images issues x

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,7 +1,4 @@
 import React, { useState, useEffect } from "react";
-import emailjs from '@emailjs/browser';
-import { useRef } from 'react';
-
 import '../scss/Projects.scss';
 import { Link } from 'react-router-dom';
 import DATA from '../data/projects';
@@ -9,9 +6,7 @@ import Loader from './sectionLoader.jsx';
 
 const Projects = () => {
   const [loading, setLoading] = useState(true);
-  const form = useRef(null);
-  const [email, setEmail] = useState('');
-  
+
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
@@ -24,56 +19,28 @@ const Projects = () => {
     return <Loader />;
   }
 
-  
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm(
-      'service_lm4juau',
-      'template_3n8awzg',
-      form.current,
-      'koPF8_4VC7XT9wEw5'
-    ).then(
-      (result) => {
-        console.log('SUCCESS!', result.text);
-      },
-      (error) => {
-        console.log('FAILED...', error.text);
-      }
-    );
-    // Clear form fields after submission (optional)
-    setEmail('');
-
-  };
   return (
     <div id="projects-container">
       <div className="subs-wrapper">
         <p>Be the first to know! Subscribe for the latest news, projects and more ...</p>
         <div className="formWrapper">
-
-          <form> ref={form} onSubmit={sendEmail}
-            <input type="email" name="user_email" placeholder='Email Address' value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required />
-            <button>
-              Subscribe
-            </button>
-          </form>
-
+          <input type="email" placeholder='Email Address' />
+          <button>
+            Subscribe
+          </button>
         </div>
       </div>
 
       <div className="project-boxes">
         <header>
-          <p>Some Things I’ve Built</p>
+          <p>Some Things I've Built</p>
         </header>
 
         {/* Mapping through fake project data */}
         {DATA.map((project, index) => (
           <div key={index} className="project-main-box">
             <Link to={`/project/${project.slug}`} className="project-link">
-              <img src={project.coverImg} alt={project.slug} className="project-image" />
+              <img src={project.coverImg} alt={project.slug} className="project-image c" />
             </Link>
             <p className="project-type">{project.tag}</p>
             <h2 className="project-title">
