6bb827ae0aca4f51cc8c420cb513b9b177a20b39
MTM <marsoumtm@gmail.com>
2024-07-12 12:15:23 +0100

    fix img 6

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,38 +1,35 @@
 const projectsData = [
   {
-    "id": 1,
-    "title": "Deverda - E-commerce Website",
-    "slug": "deverda-ecommerce-website-shopify",
-    "tag": "#Full Stack , #Shopify , #UI/UX",
+    id: 1,
+    title: "Deverda - E-commerce Website",
+    slug: "deverda-ecommerce-website-shopify",
+    tag: "#Full Stack , #Shopify , #UI/UX",
     //intro
-    "intro" : "Deverda is a sophisticated e-commerce website developed for a cosmetics brand using Shopify, a leading CMS for commerce. This project allowed me to gain valuable experience with Shopify's extensive capabilities. The main objective was to create the best possible UI/UX while implementing user behavior tracking to ensure the website could be continuously updated to meet user needs. This approach ensured a dynamic, user-centered shopping experience that adapts and improves based on user interactions and feedback.",
+    intro:
+      "Deverda is a sophisticated e-commerce website developed for a cosmetics brand using Shopify, a leading CMS for commerce. This project allowed me to gain valuable experience with Shopify's extensive capabilities. The main objective was to create the best possible UI/UX while implementing user behavior tracking to ensure the website could be continuously updated to meet user needs. This approach ensured a dynamic, user-centered shopping experience that adapts and improves based on user interactions and feedback.",
     //description
-    "description": `Deverda is a sophisticated e-commerce website developed for a cosmetics brand using Shopify, a leading CMS for commerce. This project allowed me to gain valuable experience with Shopify and its extensive capabilities. The main objective was to create the best possible UI/UX while also implementing user behavior tracking to ensure the website could be continuously updated to meet user needs.
+    description: `Deverda is a sophisticated e-commerce website developed for a cosmetics brand using Shopify, a leading CMS for commerce. This project allowed me to gain valuable experience with Shopify and its extensive capabilities. The main objective was to create the best possible UI/UX while also implementing user behavior tracking to ensure the website could be continuously updated to meet user needs.
 
     In creating Deverda, a sophisticated e-commerce platform for a renowned cosmetics brand, the utilization of Shopify, a premier CMS for commerce, was paramount. This endeavor provided me with significant experience in leveraging Shopify's comprehensive features and capabilities. The primary goal was to design an exceptional UI/UX, coupled with the integration of user behavior tracking mechanisms to facilitate ongoing updates tailored to user preferences.
     
     Developing Deverda, an advanced e-commerce site for a high-profile cosmetics brand, involved harnessing the power of Shopify, an industry-leading CMS for commerce. Through this project, I gained profound insights into Shopify's robust functionalities. The core objective was to engineer a superior UI/UX, incorporating user behavior tracking to ensure the website remains dynamically responsive to evolving user needs.`,
 
-    "coverImg": "../assets/images/projects/deverda/logo.webp",
-    "secondaryImg": "../assets/images/projects/deverda/logo.webp",
-    "date": "27 June 2023",
-    "hostLink": "https://deverda.com",
-    "githubLink": "",
-    "techIcons":
-    [
-      "/assets/images/icons/icons8-shopify-48.png", 
+    coverImg: "../assets/projects/deverda/logo.webp",
+    secondaryImg: "../assets/projects/deverda/logo.webp",
+    date: "27 June 2023",
+    hostLink: "https://deverda.com",
+    githubLink: "",
+    techIcons: ["/assets/images/icons/icons8-shopify-48.png"],
+    images: [
+      "../assets/projects/deverda/deverda(8).jpg",
+      "../assets/projects/deverda/deverda(4).jpg",
+      "../assets/projects/deverda/deverda(1).jpg",
+      "../assets/projects/deverda/deverda(2).jpg",
+      "../assets/projects/deverda/deverda(3).jpg",
+      "../assets/projects/deverda/deverda(5).jpg",
+      "../assets/projects/deverda/deverda(6).jpg",
+      "../assets/projects/deverda/deverda(7).jpg",
     ],
-    "images": [
-      "../assets/images/projects/Deverda/deverda(8).jpg",
-      "../assets/images/projects/Deverda/deverda(4).jpg",
-      "../assets/images/projects/Deverda/deverda(1).jpg",
-      "../assets/images/projects/Deverda/deverda(2).jpg",
-      "../assets/images/projects/Deverda/deverda(3).jpg",
-      "../assets/images/projects/Deverda/deverda(5).jpg",
-      "../assets/images/projects/Deverda/deverda(6).jpg",
-      "../assets/images/projects/Deverda/deverda(7).jpg"
-      
-    ]
-  }
-  ];
+  },
+];
 export default projectsData;
